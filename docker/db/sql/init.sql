e08a89a77a3479b402a012028eedbbdba0f6be53
Anorlondo448 <kanaend0605@gmail.com>
2019-12-26 23:59:19 +0900

    add mysql data directory

---
+++ docker/db/sql/init.sql
@@ -0,0 +1,17 @@
+DROP SCHEMA IF EXISTS app_database;
+CREATE SCHEMA app_database;
+USE app_database;
+
+DROP TABLE IF EXISTS member;
+
+CREATE TABLE member
+(
+  id           INT(10),
+  name     VARCHAR(40),
+  point        INT(10),
+  company  VARCHAR(255)
+);
+
+-- INSERT INTO member (id, name, point, company) VALUES (1, "YEBISU", 95, "KIRIN");
+-- INSERT INTO member (id, name, point, company) VALUES (2, "MALTS",  90, "SUNTORY");
+-- INSERT INTO member (id, name, point, company) VALUES (3, "一番搾り",  100, "KIRIN");
