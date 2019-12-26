717e72a0cc390395524b3a6f926f32816118434d
Anorlondo448 <kanaend0605@gmail.com>
2019-12-26 21:36:55 +0900

    add basic web

---
+++ web/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func topHandler(w http.ResponseWriter, r *http.Request) {
+
+	url := "http://localhost:8081/ping"
+	resp, _ := http.Get(url)
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	fmt.Fprintf(w, "web-"+string(byteArray))
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "web-pong")
+}
+
+func main() {
+	http.HandleFunc("/", topHandler)
+	http.HandleFunc("/ping", pingHandler)
+	http.ListenAndServe(":8080", nil)
+}
